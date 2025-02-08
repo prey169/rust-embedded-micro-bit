6e12c6fb553eb2b36a147ccd34850f34e759f058
prey169 <francisdrossi@gmail.com>
2025-02-08 11:23:26 -0500

    set some variables and a loop to run on the microbit

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,19 @@
+#![deny(unsafe_code)]
 #![no_std]
 #![no_main]
 
-use panic_rtt_target as _;
-use rtt_target::{rprintln, rtt_init_print};
-
 use cortex_m_rt::entry;
+use microbit as _;
+use panic_rtt_target as _;
+//use rtt_target::{rprintln, rtt_init_print};
 
 #[entry]
 fn main() -> ! {
-    rtt_init_print!();
-    rprintln!("Hello World");
+    let _y;
+    let x = 42;
+    _y = x;
+    //rtt_init_print!();
+    //rprintln!("Hello World");
+    //
     loop {}
 }
